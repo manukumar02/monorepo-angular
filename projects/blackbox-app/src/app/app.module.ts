6d6837bf6fd52fb870c59904a374a8f6e33295c4
Manu <manukumar02@gmail.com>
2021-11-23 20:08:08 +0530

    first commit

---
+++ projects/blackbox-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderLibModule } from 'header-lib';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderLibModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
